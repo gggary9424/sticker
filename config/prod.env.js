9c8e51073efc75e49c4904350e9704bf344a7f29
Gary Chen <garychen@kkbox.com>
2018-02-25 20:45:57 +0800

    Modify FB_APP_ID

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  FB_APP_ID: '"1925245824355888"',
+  FB_APP_ID: '"1477124502376307"',
   BASE_API: '"https://sticker-bot-forward.herokuapp.com/"',
 }
