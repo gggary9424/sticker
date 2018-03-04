ef2d1d8dbaa1c449ee9944d8d3224fe179df687f
Gary Chen <garychen@kkbox.com>
2018-03-04 16:30:40 +0800

    Fix

--- src/App.vue
+++ src/App.vue
@@ -14,38 +14,42 @@ export default {
     Home,
   },
   beforeCreate() {
+    const store = this.$store
     // for test
-    // return request.post('/v1/login', {})
-    //   .then((response) => {
-    //     if (response.data.token) {
-    //       this.$store.commit('setUserInfo', response.data.token)
-    //     } else {
-    //       this.$store.commit('setUserInfo', '')
-    //     }
-    //   })
-    window.extAsyncInit = function() {
-      return new Promise((resolve, reject) => {
-          return MessengerExtensions
-            .getContext(process.env.FB_APP_ID, (context) => {
-              return resolve(context)
-            }, (errorCode, errorMessage) => {
-              console.error(errorCode, errorMessage)
-
-              return reject({errorCode, errorMessage})
-            })
-        })
-        .then((context) => {
-          console.log(context)
-
-          return request.post('/v1/login', context)
-        })
-        .then(function (response) {
+    if (process.env.NODE_ENV === 'development') {
+      return request.post('/v1/login', {})
+        .then((response) => {
           if (response.data.token) {
             this.$store.commit('setUserInfo', response.data.token)
           } else {
             this.$store.commit('setUserInfo', '')
           }
         })
+    } else {
+      window.extAsyncInit = function() {
+        return new Promise((resolve, reject) => {
+            return MessengerExtensions
+              .getContext(process.env.FB_APP_ID, (context) => {
+                return resolve(context)
+              }, (errorCode, errorMessage) => {
+                console.error(errorCode, errorMessage)
+
+                return reject({errorCode, errorMessage})
+              })
+          })
+          .then((context) => {
+            console.log(context)
+
+            return request.post('/v1/login', context)
+          })
+          .then(function (response) {
+            if (response.data.token) {
+              store.commit('setUserInfo', response.data.token)
+            } else {
+              store.commit('setUserInfo', '')
+            }
+          })
+      }
     }
   },
 }
